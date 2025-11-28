eb689eb73ad740e73c6ccfcfa4b48c94ef325c24
SAKATOKU <akr.sakatoku@gmail.com>
2025-11-28 17:00:14 +0000

    Week 140の解答を追加

---
+++ answers/week140.sql
@@ -0,0 +1,98 @@
+--❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄--
+--                    ,                                                                                                                                           --
+--                 `^!J"^                                                                                                                                         --
+--                  -JLJ.                     ...........`  `...,^^-.       ./i)?/.       `,riv~`  /iiiiiiiiiii*  `'!     ::'.                                    --
+--                  .'^`.                    'BBBBBBBBBBB^ GNBBBBBBBBO"   .ABBBBHBBo.   "GBBBBBBBt BBBBBBBBBBBBB'EmBB1   "BNk].                                   --
+--                  !49S:                    <BBBBBBBBNPt`.NBBBB6(5BBBN'  gBBBm=GBBBm  ^BBBBjiBBBB-B6MNNBBBB)))i`"BBBN' .MBBB!                                    --
+--                :e99999x.                  vBBB&.       .BBBB   *BBBN, &BBBM  `GBBB  @BBBB=.KOOO.::- RBBBB      *BBBN:RNBBi                                     --
+--              `=999999999+                 7BB&@!!!!!!  .BNBN!+c$BBBj "BBBB"   BBBB  >BBBBB8r'       RBBBB       #BBBQBBB5                                      --
+--             "4999999999994_               uBBBBBBBBB0  :BBBBBBBBBNr  +BBBB"  !RBXN   "GNBBBRBo`     RBBBp        MBBBBEG                                       --
+--           'u999999999999999L'             &BBBNpEUux|  ?BBBB&BBBBo   =BQBB"  SXu&m     `r4BBBNQ     qBBB4        +BBBRR,                                       --
+--          !9999999999999999995+            BBBBP        NBBB^ !RBBBG  :BBBB4<7BBNR!  tY*- `BBqBB     ~BBB<         BBBp!              ,.              -`        --
+--          `:t999999999999999/'             BNBBP        BBBB^  ~MBBR5  ;@BGBBGBqP"  vBBBBBBBBBQ;    .-BBB<         BBBO-            _oooe7;       '/{ooe^       --
+--          ,y999999999999999993'            4A44L        4444,   ^4t`     *A444z!     .vn4444u?      .2444"        `4444-            L4A444Ay_   `=eA4444A       --
+--        `?%99999999999999999999|                                                                                                    _*Pi+^"!C1 "uF!!/*kT"       --
+--       !S99999999999999999999999S"          __________~    `'~___~       !!=/!      .__^^_.          :_^_.      `~'     ~__'      .'''-_+/vtl7>)lJv|="~,--.     --
+--       "=Y999999999999999999999i+!         vBBB@BBBBBBB .NBBBBNBBBBS     JRBBN    @BBBBBMBBH=       zBBBBB/    RHBB-   rNjaG     `""""""l{<";;""!!iA2|/////     --
+--        !P999999999999999999999A"          iBBBBBBNMRRG .BBBBMRqMBBBR    4BBBE    BBBBNMNBB&B!     |BBBBBRM`   tBBB@` _NBBB_      ^"""""l["""""////A2|//r/!     --
+--       l999999999999999999999999Pr         nBBME```     ,BBBH   YBBBQ   |ABBB`   !$BBG`  RRBB@    ^BBBRBBBBv    EBBB2~BNBB"        ;!+++l[!!!""r<<<A2?>>1"      --
+--     -P999999999999999999999999999[.       &BBBA*****'  1BNB!_*uBBBB<   AlBBB    ARBBS   vONBB   -NBBNekBBBB    .MBBBNBBB7         ^""""l["""!=<r//A2|///"      --
+--     "eoSP9999999999999999999%w4eeu^       BBBBBBBBBB2  @BBBBBBRMBg!    S6BNR    ABBB4   N@6BA   mBBR6~;B@kB!    !BBB@BG4          ^""""l["""""////A2>///"      --
+--               `.'*999L++;.                BBBBBBkAAA!  BBBBBBBBBBv     4BBRK    ABBB*  lBNBB^  cBBBNBBBBBBBT     3BBBgH`          ^""""l["""""////A2>///"      --
+--                  "999"                    NBBB         BBBB^ )BBBB<    XBBNM    ABBBDegBBBB!   RBBRNBBBBBpQN      BBBa'           _""""l]"""""////A2>///"      --
+--                  _999^                   `BBBB        `BBBB^  *BBBB"   XBBRR    6&BBBBBBB2.   UBBGBi_''EBe^B!    ^BBBG'           _""""l]"""""////A2>///"      --
+--                  ~999^                   `RRRR        -uuuu'   =5?.    oRRR$    6mRRO2z*`    .gRRRk    _RRRRA    !RRR@'            `''-!i"""""////Y/;_:'       --
+--                  `~__`                                                                                                                     ``...               --
+--                                                                                                                                                                --
+--❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄--
+
+-- このチャレンジの詳細
+-- [Week 140 - Intermediate](https://frostyfriday.org/blog/2025/06/13/week-140-intermediate/)
+
+-- このチャレンジに関連するドキュメント
+-- [AI_CLSSIFY](https://docs.snowflake.com/ja/sql-reference/functions/ai_classify)
+-- [CLASSIFY_TEXT](https://docs.snowflake.com/ja/sql-reference/functions/classify_text-snowflake-cortex)
+-- [ARRAY_AGG](https://docs.snowflake.com/ja/sql-reference/functions/array_agg)
+
+-- データベースとスキーマを選択
+USE DATABASE FROSTYFRIDAY_DB;
+USE SCHEMA WEEK140;
+
+-- 提供されたコード
+-- invoice_line_itemsテーブルを作成
+CREATE OR REPLACE TABLE invoice_line_items (
+    line_item_id INTEGER,
+    service_description STRING
+);
+-- service_categoriesテーブルを作成
+CREATE OR REPLACE TABLE service_categories (
+    category_id INTEGER,
+    category_name STRING
+);
+-- invoice_line_itemsテーブルにサンプルデータを挿入
+INSERT INTO invoice_line_items (line_item_id, service_description) VALUES
+(1, 'Deployment of Snowflake project - Phase 1'),
+(2, 'Data ingestion pipeline optimization'),
+(3, 'Security and access review for Snowflake'),
+(4, 'Ongoing data modeling support'),
+(5, 'Snowflake training session for analysts');
+-- service_categoriesテーブルにサンプルデータを挿入
+INSERT INTO service_categories (category_id, category_name) VALUES
+(1, 'Snowflake Deployment'),
+(2, 'Data Engineering'),
+(3, 'Security Review'),
+(4, 'Training'),
+(5, 'Analytics Support');
+
+-- 解法
+-- ベースになるテーブルを表示
+SELECT
+    LINE_ITEM_ID,
+    SERVICE_DESCRIPTION AS INPUT,
+FROM invoice_line_items
+-- service_categoriesテーブルにあるカテゴリを配列化する
+->>
+SELECT
+    ARRAY_AGG(CATEGORY_NAME) AS CLASSES
+FROM service_categories
+-- CROSS JOINして横並びにする
+->>
+SELECT
+    A.*,
+    B.*,
+FROM $2 AS A
+CROSS JOIN $1 AS B
+-- AI_CLASSIFYで分類する！
+->>
+SELECT
+    *,
+    AI_CLASSIFY(INPUT, CLASSES, {'output_mode': 'single'}) AS PREDICTED_CATEGORY_JSON,
+FROM $1
+-- AI_CLASSIFYだと結果がJSONで戻ってくるので、パースする
+->>
+SELECT
+    LINE_ITEM_ID,
+    INPUT,
+    CLASSES,
+    PREDICTED_CATEGORY_JSON:labels[0] AS PREDICTED_CATEGORY
+FROM $1;
